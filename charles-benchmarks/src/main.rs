419ec323f5bdcb2341cb0d7643c73a5b64ac6293
CharlesThuys <tcharles29102002@gmail.com>
2026-08-06 11:37:40 +0200

    improve benches

--- charles-benchmarks/src/main.rs
+++ charles-benchmarks/src/main.rs
@@ -1,10 +1,13 @@
 use std::{
+    collections::{HashMap, HashSet},
     ffi::OsStr,
     fs, io,
     num::NonZeroUsize,
-    path::absolute,
+    path::{PathBuf, absolute},
     time::{Duration, Instant},
 };
+
+use anyhow::{Result, anyhow};
 
 use rust_to_wasm_compiler::WasiSupport;
 use wastrumentation::{
@@ -26,19 +29,29 @@ use wasmtime_wasi::p1::{self as preview1, WasiP1Ctx};
 use chrono::Utc;
 
 const RUNS: usize = 3;
-const PROGRAM_DIR: &str = "../../wasm-benchmarks/wasm-r3-bench"; //"../../../wasm-benchmarks/wasm-r3-bench";
+const PROGRAM_DIR: &str = "../../wasm-benchmarks/wasm-r3-bench";
+const ANALYSES_DIR: &str = "input-analyses/";
 const RESULT_DIR: &str = "results/";
-const PATH_INPUT_ANLYSIS: &str = "input-analyses/forward/Cargo.toml";
 
 use serde::{Deserialize, Serialize};
 
-fn instrument(input_program: &[u8]) -> Vec<u8> {
-    let analysis_compiler = Compiler::setup_compiler().expect("Setup Rust compiler");
-    let instrumentation_compiler =
-        ASCompiler::setup_compiler().expect("Setup AssemblyScript compiler");
+fn instrument(input_program: &[u8], analysis: &str) -> Result<Vec<u8>> {
+    let analysis_hooks: HashMap<&'static str, HashSet<Hook>> = HashMap::from([
+        ("generic-apply", HashSet::from([Hook::GenericApply])),
+        ("forward", Hook::all_hooks()),
+    ]);
 
-    let source = Manifest(WasiSupport::Enabled, absolute(PATH_INPUT_ANLYSIS).unwrap());
-    let hooks = Hook::all_hooks();
+    let analysis_compiler = Compiler::setup_compiler()?;
+    let instrumentation_compiler = ASCompiler::setup_compiler()?;
+
+    let source = Manifest(
+        WasiSupport::Enabled,
+        absolute(ANALYSES_DIR.to_string() + analysis + "/Cargo.toml")?,
+    );
+    let hooks = analysis_hooks
+        .get(analysis)
+        .cloned()
+        .ok_or_else(|| anyhow::anyhow!("unknown analysis: {analysis}"))?; // Hook::all_hooks();
     let analysis = RustAnalysisSpec { source, hooks }.into();
 
     let configuration = Configuration {
@@ -47,43 +60,39 @@ fn instrument(input_program: &[u8]) -> Vec<u8> {
     };
 
     let wastrumenter = Wastrumenter::new(instrumentation_compiler.into(), analysis_compiler.into());
-    wastrumenter
+    let wastrumented = wastrumenter
         .wastrument(input_program, analysis, &configuration)
-        .expect("Wastrumentation should succeed")
+        .map_err(|error| anyhow!("Wastrumentation failed: {error:?}"))?;
+    Ok(wastrumented)
 }
 
-fn execute_on_wasmtime(program: &[u8], disable: bool) -> Duration {
+fn execute_on_wasmtime(program: &[u8], disable: bool) -> Result<Duration> {
     let mut config = Config::new();
 
     config.wasm_backtrace_max_frames(Some(NonZeroUsize::new(100).unwrap()));
 
-    let engine = Engine::new(&config).unwrap();
+    let engine = Engine::new(&config)?;
     let mut linker: Linker<WasiP1Ctx> = Linker::new(&engine);
 
-    preview1::add_to_linker_sync(&mut linker, |ctx| ctx).unwrap();
+    preview1::add_to_linker_sync(&mut linker, |ctx| ctx)?;
 
     let wasi_ctx = WasiCtxBuilder::new().inherit_stdio().build_p1();
 
     let mut store = Store::new(&engine, wasi_ctx);
-    let module = Module::from_binary(store.engine(), program).unwrap();
-    let instance = linker.instantiate(&mut store, &module).unwrap();
+    let module = Module::from_binary(store.engine(), program)?;
+    let instance = linker.instantiate(&mut store, &module)?;
 
-    let start_function = instance
-        .get_typed_func::<(), ()>(&mut store, "_start")
-        .unwrap();
+    let start_function = instance.get_typed_func::<(), ()>(&mut store, "_start")?;
 
     if disable {
-        let disable_instrumentation_function = instance
-            .get_typed_func::<i32, ()>(&mut store, "set_instrumentation")
-            .unwrap();
-        disable_instrumentation_function
-            .call(&mut store, 0)
-            .unwrap();
+        let disable_instrumentation_function =
+            instance.get_typed_func::<i32, ()>(&mut store, "set_instrumentation")?;
+        disable_instrumentation_function.call(&mut store, 0)?;
     }
 
     let start_time = Instant::now();
-    start_function.call(&mut store, ()).unwrap();
-    Instant::now() - start_time
+    start_function.call(&mut store, ())?;
+    Ok(Instant::now() - start_time)
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -92,79 +101,205 @@ struct BenchmarkResult {
     duration_ns: u128,
     program_name: String,
     instrumentation: String,
+    analysis_name: String,
+    run: u32,
+    error: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct SizeResult {
+    bytes: u32,
+    program_name: String,
+    instrumentation: String,
+    analysis_name: String,
+    error: String,
 }
 
 fn main() -> io::Result<()> {
-    let mut paths = fs::read_dir(PROGRAM_DIR)?
-        .map(|entry| entry.map(|entry| entry.path()))
-        .filter(|path| {
-            path.as_ref().unwrap().is_file()
-                && path.as_ref().unwrap().file_name() == Some(OsStr::new("factorial.wasm"))
-        })
-        .collect::<io::Result<Vec<_>>>()?;
+    let programs = load_programs()?;
+    let analyses = load_analyses()?;
 
-    paths.sort();
+    println!(
+        "{:?}",
+        analyses
+            .iter()
+            .map(|p| p.file_stem().unwrap().to_str().unwrap())
+            .collect::<Vec<_>>()
+    );
 
     let time = Utc::now();
     fs::create_dir(RESULT_DIR.to_owned() + &time.to_rfc3339())?;
 
-    for path in paths {
-        // Ignore directories and non-WASM files.
-        if !path.is_file() || path.extension().and_then(|ext| ext.to_str()) != Some("wasm") {
-            continue;
-        }
+    let mut size_wtr =
+        csv::Writer::from_path(RESULT_DIR.to_owned() + &time.to_rfc3339() + "/baseline_sizes.csv")?;
+    let mut wtr = csv::Writer::from_path(
+        RESULT_DIR.to_owned() + &time.to_rfc3339() + "/baseline_result.csv",
+    )?;
 
+    for program_path in programs {
         // The complete WASM program as raw bytes.
-        let program: Vec<u8> = fs::read(&path)?;
-        let program_name = path.file_stem().unwrap().to_str().unwrap();
+        let program: Vec<u8> = fs::read(&program_path)?;
+        let program_name = program_path.file_stem().unwrap().to_str().unwrap();
 
+        // Uninstrumented program_size
         println!("Benchmarking {} ({} bytes)", program_name, program.len());
+        size_wtr.serialize(SizeResult {
+            bytes: program.len() as u32,
+            program_name: program_name.to_string(),
+            instrumentation: "uninstrumented".to_string(),
+            analysis_name: "uninstrumented".to_string(),
+            error: "".to_string(),
+        })?;
 
-        let mut wtr =
-            csv::Writer::from_path(RESULT_DIR.to_owned() + &time.to_rfc3339() + "/result.csv")?;
-
-        let mut size = program.len();
-        // UNINSTRUMENTED
-        for _ in 0..RUNS {
-            let result = execute_on_wasmtime(&program, false);
-            wtr.serialize(BenchmarkResult {
-                duration_ms: result.as_millis(),
-                duration_ns: result.as_nanos(),
-                program_name: program_name.to_string(),
-                instrumentation: "uninstrumented".to_string(),
-            })?;
-        }
-        println!("Uninstrumented size: {}", size);
-
-        // INSTRUMENTED ON
-        for _ in 0..RUNS {
-            let instrumented = instrument(&program);
-            size = instrumented.len();
-            let result = execute_on_wasmtime(&instrumented, false);
-            wtr.serialize(BenchmarkResult {
-                duration_ms: result.as_millis(),
-                duration_ns: result.as_nanos(),
-                program_name: program_name.to_string(),
-                instrumentation: "enabled".to_string(),
-            })?;
-        }
-        println!("Instrumented Size: {}", size);
-
-        // INSTRUMENTED OFF
-        for _ in 0..RUNS {
-            let instrumented = instrument(&program);
-            size = instrumented.len();
-            let result = execute_on_wasmtime(&instrumented, true);
-            wtr.serialize(BenchmarkResult {
-                duration_ms: result.as_millis(),
-                duration_ns: result.as_nanos(),
-                program_name: program_name.to_string(),
-                instrumentation: "disabled".to_string(),
-            })?;
+        // Run uninstrumented
+        for run in 0..RUNS {
+            match execute_on_wasmtime(&program, false) {
+                Ok(result) => {
+                    wtr.serialize(BenchmarkResult {
+                        duration_ms: result.as_millis(),
+                        duration_ns: result.as_nanos(),
+                        program_name: program_name.to_string(),
+                        instrumentation: "uninstrumented".to_string(),
+                        analysis_name: "uninstrumented".to_string(),
+                        run: run as u32,
+                        error: "".to_string(),
+                    })?;
+                }
+                Err(err) => {
+                    wtr.serialize(BenchmarkResult {
+                        duration_ms: 0,
+                        duration_ns: 0,
+                        program_name: program_name.to_string(),
+                        instrumentation: "uninstrumented".to_string(),
+                        analysis_name: "uninstrumented".to_string(),
+                        run: run as u32,
+                        error: err.to_string(),
+                    })?;
+                }
+            }
         }
 
-        wtr.flush()?; // Ensure data is written
+        for analysis_path in &analyses {
+            let analysis_name = analysis_path.file_stem().unwrap().to_str().unwrap();
+            let mut instrumented = vec![];
+
+            // Instrumented Program Size
+            match instrument(&program, analysis_name) {
+                Ok(wastrumented) => {
+                    instrumented = wastrumented;
+                    println!(
+                        "Instrumented {} ({} bytes)",
+                        program_name,
+                        instrumented.len()
+                    );
+                    size_wtr.serialize(SizeResult {
+                        bytes: instrumented.len() as u32,
+                        program_name: program_name.to_string(),
+                        instrumentation: "baseline".to_string(),
+                        analysis_name: analysis_name.to_string(),
+                        error: "".to_string(),
+                    })?;
+                }
+                Err(err) => {
+                    size_wtr.serialize(SizeResult {
+                        bytes: 0,
+                        program_name: program_name.to_string(),
+                        instrumentation: "baseline".to_string(),
+                        analysis_name: analysis_name.to_string(),
+                        error: err.to_string(),
+                    })?;
+                    continue;
+                }
+            }
+
+            // Run instrumented
+            for run in 0..RUNS {
+                match execute_on_wasmtime(&instrumented, false) {
+                    Ok(result) => {
+                        wtr.serialize(BenchmarkResult {
+                            duration_ms: result.as_millis(),
+                            duration_ns: result.as_nanos(),
+                            program_name: program_name.to_string(),
+                            instrumentation: "baseline".to_string(),
+                            analysis_name: analysis_name.to_string(),
+                            run: run as u32,
+                            error: "".to_string(),
+                        })?;
+                    }
+                    Err(err) => {
+                        wtr.serialize(BenchmarkResult {
+                            duration_ms: 0,
+                            duration_ns: 0,
+                            program_name: program_name.to_string(),
+                            instrumentation: "baseline".to_string(),
+                            analysis_name: analysis_name.to_string(),
+                            run: run as u32,
+                            error: err.to_string(),
+                        })?;
+                    }
+                }
+            }
+
+            // INSTRUMENTED OFF
+            /*
+            for run in 0..RUNS {
+                match execute_on_wasmtime(&instrumented, true) {
+                    Ok(result) => {
+                        wtr.serialize(BenchmarkResult {
+                            duration_ms: result.as_millis(),
+                            duration_ns: result.as_nanos(),
+                            program_name: program_name.to_string(),
+                            instrumentation: "disabled".to_string(),
+                            analysis_name: analysis_name.to_string(),
+                            run: run as u32,
+                            error: "".to_string(),
+                        })?;
+                    }
+                    Err(err) => {
+                        wtr.serialize(BenchmarkResult {
+                            duration_ms: 0,
+                            duration_ns: 0,
+                            program_name: program_name.to_string(),
+                            instrumentation: "disabled".to_string(),
+                            analysis_name: analysis_name.to_string(),
+                            run: run as u32,
+                            error: err.to_string(),
+                        })?;
+                    }
+                }
+            }*/
+
+            wtr.flush()?; // Ensure data is written
+        }
     }
-
     Ok(())
+}
+
+fn load_programs() -> io::Result<Vec<PathBuf>> {
+    let mut paths = fs::read_dir(PROGRAM_DIR)?
+        .map(|entry| entry.map(|entry| entry.path()))
+        .filter(|path| {
+            let p = path.as_ref().unwrap();
+            p.is_file()
+                && (
+                    p.file_name() == Some(OsStr::new("factorial.wasm"))
+                        || p.file_name() == Some(OsStr::new("game-of-life.wasm"))
+                    // || p.file_name() == Some(OsStr::new("guiicons.wasm"))
+                )
+                && p.extension().and_then(|ext| ext.to_str()) == Some("wasm")
+        })
+        .collect::<io::Result<Vec<_>>>()?;
+
+    paths.sort();
+    Ok(paths)
+}
+
+fn load_analyses() -> io::Result<Vec<PathBuf>> {
+    let mut paths = fs::read_dir(ANALYSES_DIR)?
+        .map(|entry| entry.map(|entry| entry.path()))
+        .filter(|path| path.as_ref().unwrap().is_dir())
+        .collect::<io::Result<Vec<_>>>()?;
+
+    paths.sort();
+    Ok(paths)
 }
